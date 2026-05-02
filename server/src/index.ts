0c689cfc2fab44fc5892eedde0dcf03852b7f340
bristolkon <bristolkon3802@gmail.com>
2026-05-02 12:16:23 +0900

    [2026.05.02] 플래시 카드 덱 생성

--- server/src/index.ts
+++ server/src/index.ts
@@ -38,10 +38,88 @@ export default {
 			};
 		});
 
-		await env.FLASHCARDS_KV.put('hello', 'world');
+		await env.FLASHCARDS_KV.put('hello', '{"hello": "world"}');
+
+		await env.FLASHCARDS_KV.get('hello', 'json');
 
 		// create deck -> AI model이 카드(Flashcard) 덱을 만듬
 		// ex) "나는 영어를 배우고 싶으니간, 20개의 단어를 줘"  AI model이 data를 만들어 줌
+		registerAppTool(
+			server,
+			'create-deck',
+			{
+				title: 'Create Deck',
+				description:
+					'이를 사용하여 스터디용 플래시 카드 덱을 만듭니다. 앞면(질문)과 뒷면(답변)에 힌트를 포함한 20장의 카드를 생성합니다. 이 도구를 사용하기 전에 사용자에게 사용자 이름을 물어보세요.',
+				inputSchema: {
+					usernaem: z.string().describe('사용자의 사용자 이름입니다. 도구를 사용하기 전에 이를 요청하세요.'),
+					title: z.string().describe("덱의 제목: 예를 들어 '리액트 펀더멘털'"),
+					description: z.string().describe('이 덱이 다루는 내용에 대한 간략한 설명'),
+					cards: z
+						.array(
+							z.object({
+								front: z.string().describe('질문 또는 프롬프트'),
+								back: z.string().describe('정답'),
+								hint: z.string().describe('카드에 대한 힌트'),
+							}),
+						)
+						.min(10)
+						.max(20)
+						.describe('플래시 카드 배열 (20장 목표).'),
+				},
+				annotations: {
+					readOnlyHint: false,
+				},
+				_meta: {
+					ui: {
+						resourceUri: WIDGET_URI,
+					},
+				},
+			},
+			async ({ title, description, cards, usernaem }) => {
+				const cardsWithIds = cards.map((card, index) => ({
+					id: `card-${Date.now()}-${index}`,
+					status: 'now',
+					...card,
+				}));
+				const deck = {
+					id: `deck-${Date.now()}`,
+					title,
+					description,
+					cards: cardsWithIds,
+					createdAt: new Date().toISOString(),
+				};
+
+				const decksKey = `user:${usernaem}:decks`;
+
+				// 카드뭉치 data를 FLASHARDS_KV에 저장
+				await env.FLASHCARDS_KV.put(`user:${usernaem}:deck:${deck}`, JSON.stringify(deck));
+
+				// usernaem을 사용해서 카드뭉치 조회
+				const existingIds = await env.FLASHCARDS_KV.get<string[]>(decksKey, 'json');
+
+				// 조회된 카드뭉치가 있다면 추가, 없다면 [] 예) `user:bingstar:decks` -> ['deck_1', 'deck_2', 'deck_3'] || 만약 없다면 빈 []
+				const deckIds = existingIds || [];
+
+				// 조회된 카드뭉치와 이전 카드뭉치를 deckIds에 묶음
+				// push에 오류가 뜨는 이유는 existingIds를 알아보지 못하고 (unknown) deckIds를 빈 object로 취급해서임. 그래서 get<string[]>를 명시
+				// 처음 접속할때는 데이터가 비어있는 상태이기 때문에 <string[]> 명시
+				deckIds.push(deck.id);
+
+				// 방금만든 카드뭉치 (decksKey)와, 새로 만든 뭉치가 추가된 (deckIds) 페어를 FLASHCARDS_KV에 추가
+				await env.FLASHCARDS_KV.put(decksKey, JSON.stringify(deckIds));
+
+				return {
+					content: [
+						{
+							type: 'text',
+							text: `${cards.length} 플래시카드로 ${title} 덱을 만들었습니다`,
+						},
+					],
+					structuredContent: { deck, usernaem },
+				};
+			},
+		);
 
 		// list decks -> 카드 리스트가 있다면 모두 볼 수 있게 해주는 tool
 
